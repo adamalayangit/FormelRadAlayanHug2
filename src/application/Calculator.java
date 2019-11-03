b28cf569e3210182c0830738a54402e53ca9fe46
Adam Alayan <adam.alayan@outlook.com>
2019-11-03 16:25:03 +0100

    Baseproject has to be integrated.

---
+++ src/application/Calculator.java
@@ -0,0 +1,55 @@
+package application;
+
+/**
+ * Berechnet das Formelrad
+ * @author Peter Rutschmann
+ * @version 13.09.2018
+ */
+public class Calculator {
+	private double leistung;
+	private double spannung;
+	private double strom;
+	private double widerstand;
+	
+	public Calculator(double leistung, double spannung, double strom, double widerstand) {
+		super();
+		this.leistung = leistung;
+		this.spannung = spannung;
+		this.strom = strom;
+		this.widerstand = widerstand;
+	}
+	
+	public double getLeistung() {
+		return leistung;
+	}
+	
+	public double getSpannung() {
+		return spannung;
+	}
+
+	public double getStrom() {
+		return strom;
+	}
+
+	public double getWiderstand() {
+		return widerstand;
+	}
+
+	@Override
+	public String toString() {
+		return "Calculator [leistung=" + leistung + 
+				", spannung=" + spannung + 
+				", strom=" + strom + 
+				", widerstand="	+ widerstand + "]";
+	}
+
+	public void calculate() {
+		/* Hier auf Grund der vorhanden Werte entscheiden
+		 * welche Methode unten aufgerufen werden muss.
+		 */
+	}
+	
+	/* Hier die Methoden mit den Formlen hinzufügen
+	 */
+	
+}
